a1fe9cd7cf171e0702b488c041ee69113d94c4d1
Pradyumna Das <pradas@microsoft.com>
2018-11-23 16:16:51 +0530

    Changing Aad auth to use certificates instead of client secrets.

--- SampleCPMProject/CpmClient/AadAuthentication.cs
+++ SampleCPMProject/CpmClient/AadAuthentication.cs
@@ -1,17 +1,19 @@
 ﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using System.Configuration;
+using System.Security.Cryptography.X509Certificates;
 
 namespace SampleCPMProject
 {
     internal static class AadAuthentication
     {
         static AuthenticationContext context;
-        static ClientCredential credentials;
+        static IClientAssertionCertificate credentials;
 
         static AadAuthentication()
         {
+            X509Certificate2 cert = GetCertificateFromStore(ConfigurationManager.AppSettings["Thumbprint"]);
             context = new AuthenticationContext(ConfigurationManager.AppSettings["AadInstance"] + ConfigurationManager.AppSettings["TenantId"]);
-            credentials = new ClientCredential(ConfigurationManager.AppSettings["ClientId"], ConfigurationManager.AppSettings["ClientSecret"]);
+            credentials = new ClientAssertionCertificate(ConfigurationManager.AppSettings["ClientId"], cert);
         }
 
         public static string GetAccessTokenForCpmApi()
@@ -35,6 +37,27 @@ namespace SampleCPMProject
             } while (authenticationResult == null);
 
             return authenticationResult.AccessToken;
+        }
+
+        private static X509Certificate2 GetCertificateFromStore(string certThumbprint)
+        {
+            var store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.OpenExistingOnly | OpenFlags.ReadOnly);
+
+            var certs = store.Certificates.Find(X509FindType.FindByThumbprint, certThumbprint, false);
+
+            if (certs.Count == 1)
+            {
+                return certs[0];
+            }
+            if (certs.Count > 1)
+            {
+                throw new System.Exception($"More than one certificate with thumbprint {certThumbprint} " +
+                                    "found in LocalMachine store location");
+            }
+
+            throw new System.Exception($"No certificate found with thumbprint {certThumbprint} " +
+                                "in LocalMachine store location");
         }
     }
 }
